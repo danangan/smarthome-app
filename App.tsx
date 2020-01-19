491f7ba658af1a1ea513237652a950b3d3f9c6ba
Danang Nur <danang.arbansa@personio.de>
2020-01-19 23:34:08 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -9,109 +9,191 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text, ScrollView} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const shadow = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 3.84,
+};
 
-declare var global: {HermesInternal: null | {}};
+const colorPalette = {
+  light: '#45BF55',
+  lighter: '#97ED8A',
+  main: '#168039',
+  dark: '#044D29',
+  darker: '#00261C',
+  grey: '#bbb',
+  lightGrey: '#efefef',
+  black: '#222',
+  white: '#fff',
+};
+
+const rooms = [
+  {
+    key: 'livingRoom',
+    name: 'Living Room',
+  },
+  {
+    key: 'bedroom',
+    name: 'Bedroom',
+  },
+  {
+    key: 'kitchen',
+    name: 'Kitchen',
+  },
+];
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <SafeAreaView style={styles.app}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>
+          Hi, <Text style={fontStyles.titleBold}>Danang!</Text>
+        </Text>
+      </View>
+      <View style={styles.overview}>
+        <Text style={[styles.energyConsumption, fontStyles.body]}>
+          Your energy consumption today
+        </Text>
+        <Text style={styles.energyConsumptionSum}>120 kwH</Text>
+      </View>
+      <View style={styles.graphContainer} {...shadow}>
+        <Text>Graph goes here</Text>
+      </View>
+      <ScrollView horizontal style={styles.scrollMenu}>
+        {rooms.map((item, index) => (
+          <View
+            {...shadow}
+            key={item.key}
+            style={[
+              styles.scrollableMenu,
+              styles[item.key],
+              index === 0 ? {marginLeft: 24} : undefined,
+            ]}>
+            <Text style={styles.scrollMenuTitle}>{item.name}</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        ))}
+        <View style={[styles.scrollableMenu, styles.addMore]}>
+          <Text style={styles.scrollMenuTitle}>Add More</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.menuBar}>
+        <View>
+          <Text>Analytics</Text>
+        </View>
+        <View>
+          <Text>Profile</Text>
+        </View>
+        <View>
+          <Text>Setup a new device</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+const fontStyles = StyleSheet.create({
+  title: {
+    fontFamily: 'Raleway-Light',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  titleBold: {
+    fontFamily: 'Raleway-Regular',
   },
   body: {
-    backgroundColor: Colors.white,
+    fontFamily: 'OpenSans-Light',
   },
-  sectionContainer: {
-    marginTop: 32,
+});
+
+const styles = StyleSheet.create({
+  app: {
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: colorPalette.white,
+  },
+  avatar: {
+    height: 30,
+    width: 30,
+    borderRadius: 24,
+  },
+  headerText: {
+    color: colorPalette.light,
+    fontSize: 30,
+    fontFamily: 'Raleway-Light',
+  },
+  header: {
     paddingHorizontal: 24,
+    marginTop: 24,
+    marginBottom: 12,
   },
-  sectionTitle: {
+  overview: {
+    backgroundColor: 'white',
+    marginHorizontal: 24,
+    marginBottom: 36,
+    borderRadius: 24,
+  },
+  overviewTitle: {
+    fontFamily: 'Raleway-SemiBold',
+    marginBottom: 6,
+    color: colorPalette.black,
+  },
+  energyConsumption: {
+    fontFamily: 'OpenSans-Regular',
+    color: colorPalette.black,
+    fontSize: 20,
+  },
+  energyConsumptionSum: {
     fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+    fontFamily: 'OpenSans-Bold',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  graphContainer: {
+    marginHorizontal: 24,
+    backgroundColor: colorPalette.light,
+    height: 240,
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  scrollMenu: {
+    maxHeight: 260,
+    marginVertical: 24,
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  scrollableMenu: {
+    height: 240,
+    width: 200,
+    borderRadius: 24,
+    marginLeft: 18,
+    justifyContent: 'flex-end',
+    padding: 12,
+  },
+  scrollMenuTitle: {
+    color: 'white',
+    fontFamily: 'Raleway-Bold',
+    fontSize: 24,
+  },
+  livingRoom: {
+    backgroundColor: colorPalette.lighter,
+  },
+  bedroom: {
+    backgroundColor: colorPalette.light,
+  },
+  kitchen: {
+    backgroundColor: colorPalette.main,
+  },
+  addMore: {
+    backgroundColor: colorPalette.darker,
+    marginRight: 24,
+  },
+  menuBar: {
+    backgroundColor: colorPalette.main,
+    padding: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 1,
   },
 });
 
